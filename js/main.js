9edbb47a3061abf6f7e4ec1ed259ee9b619e81ca
Chi Thuan <chithuandev@gmail.com>
2020-08-21 23:27:48 +0700

    cap nhat san pham va menuRespon

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ $(document).ready(function () {
     $('.btn__menu').click(function (e) { 
         e.preventDefault();
         $('.btn__menu').toggleClass('active');
+        $('.WrapAllPage').toggleClass('moveNav');
+        $('.header__menu ul').toggleClass('active');
+    });
+
+    $('.overlayMenu').click(function (e) { 
+        e.preventDefault();
+        $('.btn__menu').toggleClass('active');
+        $('.WrapAllPage').toggleClass('moveNav');
         $('.header__menu ul').toggleClass('active');
     });
 });
