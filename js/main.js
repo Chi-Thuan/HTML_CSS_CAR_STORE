b8e7418dc46943383438e83a46ec4c8982381293
Chi Thuan <chithuandev@gmail.com>
2020-08-24 03:15:55 +0700

    hoan thanh chuc nang dang ky, dang nhap

--- js/main.js
+++ js/main.js
@@ -12,4 +12,28 @@ $(document).ready(function () {
         $('.WrapAllPage').toggleClass('moveNav');
         $('.header__menu ul').toggleClass('active');
     });
+
+    // validate email
+    function validateEmail(email) {
+        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+    }
+
+    $('.submitForm').click(function (e) { 
+        e.preventDefault();
+        var name = $('input[name="name"]').val();
+        var email = $('input[name="email"]').val();
+        var subject = $('input[name="subject"]').val();
+        var mess = $('textarea[name="mess"]').val();
+
+        if(name.length > 0 && validateEmail(email)){
+            alert('Chúng tôi đã nhận được thông tin của bạn. Chúng tôi sẽ chủ động liên lạc với bạn trong thời gian sớm nhất !!!')
+            $('input[name="name"]').val('');
+            $('input[name="email"]').val('');
+            $('input[name="subject"]').val('');
+            $('textarea[name="mess"]').val('');
+        }else{
+            alert('Vui lòng nhập lại !!!')
+        }
+    });
 });
